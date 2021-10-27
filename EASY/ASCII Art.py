4bcdfd23109fd9fa1c6bd87a048291ad5c0e9124
augtal <augustas.talacka@gmail.com>
2021-10-27 15:59:28 +0300

    ASCII Art - Completed

--- EASY/ASCII Art.py
+++ EASY/ASCII Art.py
@@ -4,13 +4,42 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-l = int(input())
-h = int(input())
-t = input()
+# Inputs:
+ABCs = [
+" #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ",
+"# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # ",
+"### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## ",
+"# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       ",
+"# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  "
+] # the string of characters ABCDEFGHIJKLMNOPQRSTUVWXYZ? Represented in ASCII art.
+
+l = 4 # the width L of a letter represented in ASCII art. All letters are the same width.
+h = 5 # the height H of a letter represented in ASCII art. All letters are the same height.
+t = "AB!!CD" # The line of text T, composed of N ASCII characters.
+
+# Code
+letters = []
 for i in range(h):
-    row = input()
+    row = ABCs[i]
+    amount = int(len(row) / l)
+    characters = []
+    for j in range(int(len(row) / l)):
+        characters.append(row[j*l:j*l+l])
+    letters.append(characters)
 
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+answer = [""] * h
+t = t.upper()
 
-print("answer")
+for i in range(len(t)):
+    letter = t[i]
+    if letter.isalpha():
+        letterACII = ord(letter) - 65 # A starts at 65, Z is 90
+    else:
+        letterACII = -1
+    
+    for j in range(h):
+        answer[j] = answer[j] + letters[j][letterACII]
+        
+
+for i in range(h):
+    print(answer[i])
