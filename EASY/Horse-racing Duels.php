5fe9e929e8e3a25ad7bd4436441650ed61cf4095
augtal <augustas.talacka@gmail.com>
2021-10-28 16:10:34 +0300

    Horse-racing Duels - Completed

--- EASY/Horse-racing Duels.php
+++ EASY/Horse-racing Duels.php
@@ -5,13 +5,26 @@
  **/
 
 fscanf(STDIN, "%d", $N);
+$horses = array();
 for ($i = 0; $i < $N; $i++)
 {
     fscanf(STDIN, "%d", $pi);
+    array_push($horses, $pi);
 }
 
+sort($horses);
+$min = $horses[count($horses)-1];
+
+for ($i = 1; $i < count($horses); $i++)
+{
+    $value = $horses[$i] - $horses[$i-1];
+    if($value < $min)
+    {
+        $min = $value;
+    }
+}
+
+echo($min);
 // Write an answer using echo(). DON'T FORGET THE TRAILING \n
 // To debug: error_log(var_export($var, true)); (equivalent to var_dump)
-
-echo("answer\n");
 ?>
